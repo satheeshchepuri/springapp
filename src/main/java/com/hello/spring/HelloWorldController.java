04a0c104def0aad0ad4bccad00a9df1d2aa5c7c4
satheeshchepuri <chepurusatheesh@gmail.com>
2018-08-03 09:57:40 +0530

    first commit

---
+++ src/main/java/com/hello/spring/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.hello.spring;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+   @RequestMapping("/hello")
+   public String sayHello() {
+      return "Hello Spring Boot!!";
+   }
+}
